26735eb2aa118ab367938acecf747a3e1f36e748
Olatunbosun Oladayo <tubosund@gmail.com>
2024-01-11 15:49:46 -0800

    ss

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 def can_unlock_all(boxes):
     """
     Determines if all boxes can be opened using the available keys.
@@ -12,16 +10,19 @@ def can_unlock_all(boxes):
         True if all boxes can be opened, False otherwise.
     """
 
-    keys = {0}  # Start with the key for box 0
-    opened_boxes = 0
+    n = len(boxes)
+    if n == 0:
+        return False
 
-    while keys:
-        new_keys = set()
-        for key in keys:
-            if key < len(boxes):  # Check if key is valid for a box
-                opened_boxes += 1
-                new_keys |= set(boxes[key])  # Add keys found in the opened box
-        keys = new_keys - keys  # Update keys with newly found, unused keys
+    visited = [False] * n
+    visited[0] = True  # The first box is unlocked
+    keys_stack = boxes[0]
 
-    return opened_boxes == len(boxes)
+    while keys_stack:
+        key = keys_stack.pop()
+        if 0 <= key < n and not visited[key]:
+            visited[key] = True
+            keys_stack.extend(boxes[key])
+
+    return all(visited)
 
