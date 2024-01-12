cf99d9b267eff58d47fd6d3bdd2734f468d776de
Olatunbosun Oladayo <tubosund@gmail.com>
2024-01-11 16:05:50 -0800

    gf

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,23 +4,18 @@
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened using the available keys.
-
     Args:
         boxes: A list of lists, where each inner list represents the keys
                contained in a box.
-
     Returns:
         True if all boxes can be opened, False otherwise.
     """
-
     n = len(boxes)
     if n == 0:
         return False
-
     visited = [False] * n
     visited[0] = True  # The first box is unlocked
     keys_stack = boxes[0]
-
     while keys_stack:
         key = keys_stack.pop()
         if 0 <= key < n and not visited[key]:
