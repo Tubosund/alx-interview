4f159e2693944bc51d3374be2adfda8a1aa7d69d
Olatunbosun Oladayo <tubosund@gmail.com>
2024-01-18 03:06:43 -0800

    kk

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
-
 def minOperations(n):
     """
     Calculates the minimum number of operations (Copy All and Paste) required to
     achieve exactly n H characters in a text file, starting with a single H.
+
     Args:
         n: The target number of H characters.
+
     Returns:
         The minimum number of operations required, or 0 if n is not achievable.
     """
